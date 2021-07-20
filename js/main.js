2b763a25aa288f0d747b144a9b37883433e1160c
Dario Bosco <dario_bosco@protonmail.com>
2021-07-20 13:35:35 +0200

    Esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
+var surnames = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+
 //1. chiedi all’utente il cognome
+var userSurname = prompt('Inserisci il tuo cognome');
+
+//Inserisco anche la prima lettera dell'input maiuscola
+var userSurname = userSurname.charAt(0).toUpperCase() + userSurname.slice(1);
+
 //2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+surnames.push(userSurname);
+console.log(surnames);
+
 //3. stampa la lista ordinata alfabeticamente
+var sortedSurnames = surnames.sort();
+console.log(surnames);
+
 //4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+console.log('La posizione del cognome: ', surnames.indexOf(userSurname) + 1);
